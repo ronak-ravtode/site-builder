2d5892193b59fcfb7985fd2965fce310efda802e
ronak-ravtode <ronakravtode001@gmail.com>
2026-05-01 10:05:58 +0530

    Add OpenRouter headers

--- server/configs/openai.ts
+++ server/configs/openai.ts
@@ -1,8 +1,12 @@
 import OpenAI from 'openai';
 
 const openai = new OpenAI({
-  baseURL: "https://generativelanguage.googleapis.com/v1beta/openai/",
+  baseURL: "https://openrouter.ai/api/v1",
   apiKey: process.env.AI_API_KEY,
+  defaultHeaders: {
+    "HTTP-Referer": "https://site-builder.onrender.com", // Optional, for OpenRouter rankings
+    "X-Title": "AiSiteBuilder", // Optional
+  }
 });
 
 export default openai;
